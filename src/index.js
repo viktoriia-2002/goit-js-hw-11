b3d3d5a4b2decc254b9ab40e79fd3743ba0ca853
viktoriia-2002 <92129770+viktoriia-2002@users.noreply.github.com>
2023-05-06 23:55:30 -0400

    in progress

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import './css/style.css';
-//import API from './fetchImages';
 import Notiflix from 'notiflix';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import axios from 'axios';
-
 
 const axios = require('axios').default;
 const BASE_URL = 'https://pixabay.com/api/';
@@ -11,35 +9,40 @@ const API_KEY = '36126930-7b2057d774b58ed23a3e8d721';
 
 const refs = {
   form: document.querySelector('#search-form'),
-  input: document.querySelector('.searchQuery'),
+  input: document.querySelector('[name="searchQuery"]'),
   gallery: document.querySelector('.gallery'),
   loadMoreBtn: document.querySelector('.load-more'),
 };
 
 refs.form.addEventListener('submit', onSubmit);
 
-function onSubmit(){
-    const imageElMarkup =  ` <div class="photo-card">
-    <img src="" alt="" loading="lazy" />
-    <div class="info">
-      <p class="info-item">
-        <b>Likes</b>
-      </p>
-      <p class="info-item">
-        <b>Views</b>
-      </p>
-      <p class="info-item">
-        <b>Comments</b>
-      </p>
-      <p class="info-item">
-        <b>Downloads</b>
-      </p>
-    </div>
-  </div> `
-    gallery.insertAdjacentHTML('beforeend',imageElMarkup)
+function onSubmit(event) {
+  event.preventDefault();
+  refs.gallery.innerHTML = '';
+
+  const searchQuery = refs.input.value.trim();
+
+  if (searchQuery === '') {
+    return;
+  }
+
+  getImages(searchQuery)
+    .then(images => {
+      if (images.length === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        renderImages(images);
+      }
+    })
+    .catch(error => {
+      console.error(error);
+      Notiflix.Notify.failure(
+        'An error occurred while fetching images. Please try again later.'
+      );
+    });
 }
-
-
 
 async function getImages() {
   try {
@@ -56,5 +59,53 @@ async function getImages() {
     return response;
   } catch (error) {
     console.error(error);
+    throw error;
   }
 }
+
+async function getImages(searchQuery) {
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: searchQuery,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    });
+
+    return response.data.hits;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+function renderImages(images) {
+  const imageElMarkup = images.map(image => {
+    return `
+        <div class="photo-card">
+          <img src="${image.webformatURL}" alt="${image.tags}"  width="500" loading="lazy" />
+          <div class="info">
+            <p class="info-item">
+              <b>Likes:</b> ${image.likes}
+            </p>
+            <p class="info-item">
+              <b>Views:</b> ${image.views}
+            </p>
+            <p class="info-item">
+              <b>Comments:</b> ${image.comments}
+            </p>
+            <p class="info-item">
+              <b>Downloads:</b> ${image.downloads}
+            </p>
+          </div>
+        </div>`;
+  });
+
+  refs.gallery.insertAdjacentHTML('beforeend', imageElMarkup.join(''));
+}
+
+
+
