8cbb94d5381024a07229ff4e10113e35acffd91b
viktoriia-2002 <92129770+viktoriia-2002@users.noreply.github.com>
2023-05-07 00:20:42 -0400

    in progress

--- src/index.js
+++ src/index.js
@@ -1,7 +1,148 @@
+// import './css/style.css';
+// import Notiflix from 'notiflix';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import axios from 'axios';
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// const axios = require('axios').default;
+// const BASE_URL = 'https://pixabay.com/api/';
+// const API_KEY = '36126930-7b2057d774b58ed23a3e8d721';
+
+// const refs = {
+//   form: document.querySelector('#search-form'),
+//   input: document.querySelector('[name="searchQuery"]'),
+//   gallery: document.querySelector('.gallery'),
+//   loadMoreBtn: document.querySelector('.load-more'),
+// };
+
+// refs.form.addEventListener('submit', onSubmit);
+
+// function onSubmit(event) {
+//   event.preventDefault();
+//   refs.gallery.innerHTML = '';
+
+//   const searchQuery = refs.input.value.trim();
+
+//   if (searchQuery === '') {
+//     return;
+//   }
+
+//   getImages(searchQuery)
+//     .then(images => {
+//       if (images.length === 0) {
+//         Notiflix.Notify.failure(
+//           'Sorry, there are no images matching your search query. Please try again.'
+//         );
+//       } else {
+//         renderImages(images);
+//       }
+//     })
+//     .catch(error => {
+//       console.error(error);
+//       Notiflix.Notify.failure(
+//         'An error occurred while fetching images. Please try again later.'
+//       );
+//     });
+// }
+
+// async function getImages() {
+//   try {
+//     const response = await axios.get(BASE_URL, {
+//       params: {
+//         ID: API_KEY,
+//         q: searchQuery,
+//         image_type: 'photo',
+//         orientation: 'horizontal',
+//         safesearch: true,
+//       },
+//     });
+//     console.log(response);
+//     return response;
+//   } catch (error) {
+//     console.error(error);
+//     throw error;
+//   }
+// }
+
+// async function getImages(searchQuery) {
+//   try {
+//     const response = await axios.get(BASE_URL, {
+//       params: {
+//         key: API_KEY,
+//         q: searchQuery,
+//         image_type: 'photo',
+//         orientation: 'horizontal',
+//         safesearch: true,
+//       },
+//     });
+
+//     return response.data.hits;
+//   } catch (error) {
+//     console.error(error);
+//     throw error;
+//   }
+// }
+
+// function renderImages(images) {
+//   const imageElMarkup = images.map(image => {
+//     return `
+//         <div class="photo-card">
+//           <img src="${image.webformatURL}" alt="${image.tags}"  width="500" loading="lazy" />
+//           <div class="info">
+//             <p class="info-item">
+//               <b>Likes:</b> ${image.likes}
+//             </p>
+//             <p class="info-item">
+//               <b>Views:</b> ${image.views}
+//             </p>
+//             <p class="info-item">
+//               <b>Comments:</b> ${image.comments}
+//             </p>
+//             <p class="info-item">
+//               <b>Downloads:</b> ${image.downloads}
+//             </p>
+//           </div>
+//         </div>`;
+//   });
+
+//   refs.gallery.insertAdjacentHTML('beforeend', imageElMarkup.join(''));
+// }
+
+
+// var lightbox = new SimpleLightbox(".gallery a", {
+//     captionDelay: "250",
+//     captionsData: "alt",
+//   });
+  
+//   document.body.addEventListener("keydown", (e) => {
+//     if (e.key === "Escape") {
+//       instance.close();
+//     }
+//   });
+  
+//   console.log(imageElMarkup);
+
+
+//   var lightbox = new SimpleLightbox(".gallery a", {
+//     captionDelay: "250",
+//     captionsData: "alt",
+//   });
+  
+//   document.body.addEventListener("keydown", (e) => {
+//     if (e.key === "Escape") {
+//       instance.close();
+//     }
+//   });
+  
+//   console.log(galleryItems); 
+
 import './css/style.css';
 import Notiflix from 'notiflix';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import axios from 'axios';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const axios = require('axios').default;
 const BASE_URL = 'https://pixabay.com/api/';
@@ -15,6 +156,10 @@ const refs = {
 };
 
 refs.form.addEventListener('submit', onSubmit);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
 
 function onSubmit(event) {
   event.preventDefault();
@@ -34,6 +179,7 @@ function onSubmit(event) {
         );
       } else {
         renderImages(images);
+        lightbox.refresh(); // Refresh SimpleLightbox after rendering new images
       }
     })
     .catch(error => {
@@ -85,27 +231,26 @@ async function getImages(searchQuery) {
 function renderImages(images) {
   const imageElMarkup = images.map(image => {
     return `
-        <div class="photo-card">
-          <img src="${image.webformatURL}" alt="${image.tags}"  width="500" loading="lazy" />
-          <div class="info">
-            <p class="info-item">
-              <b>Likes:</b> ${image.likes}
-            </p>
-            <p class="info-item">
-              <b>Views:</b> ${image.views}
-            </p>
-            <p class="info-item">
-              <b>Comments:</b> ${image.comments}
-            </p>
-            <p class="info-item">
-              <b>Downloads:</b> ${image.downloads}
-            </p>
+        <a href="${image.largeImageURL}">
+          <div class="photo-card">
+            <img src="${image.webformatURL}" alt="${image.tags}"  width="500" loading="lazy" />
+            <div class="info">
+              <p class="info-item">
+                <b>Likes:</b> ${image.likes}
+              </p>
+              <p class="info-item">
+                <b>Views:</b> ${image.views}
+              </p>
+              <p class="info-item">
+                <b>Comments:</b> ${image.comments}
+              </p>
+              <p class="info-item">
+                <b>Downloads:</b> ${image.downloads}
+              </p>
+            </div>
           </div>
-        </div>`;
+        </a>`;
   });
 
   refs.gallery.insertAdjacentHTML('beforeend', imageElMarkup.join(''));
 }
-
-
-
